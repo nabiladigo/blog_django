4434ecc7f7d891c83612816c13db4979887938ed
nabiladigo <nabiladigo91@gmail.com>
2022-08-23 22:13:39 -0400

    adding post and post detail

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.Home.as_view(), name="home"), 
+    # path('', views.Home.as_view(), name="home"), 
+    path('posts/', views.Post.as_view(), name="post"),
+    path('posts/<int:pk>/', views.PostDetail.as_view(), name="post_detail"),
+
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView  
-
-
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 # Create your views here.
 
 class Home(TemplateView):
     template_name = "home.html"
+
+class Post(ListView):
+    model = Post
+    template_name = 'post.html'
+
+
+class PostDetail(DetailView):
+    model= Post
+    template_name = 'post_detail.html'
+
